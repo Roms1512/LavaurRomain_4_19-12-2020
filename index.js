36a6422521d934d89b978483f09f313cdf059c07
Romain Lavaur <romainlavaur@hotmail.com>
2021-01-06 02:50:45 +0100

    commit-5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // On récupere les données du serveur 
 // avec la methode fetch
 
+
 // on créer une fonction asyncrone ici 'getAllCamera'
 // et on on cherche (Fetch) le serveur
 
@@ -8,8 +9,26 @@
 // on retourn le dossier json
 // sinon on fait un console.error
 
-const getAllCamera = async function() {
+const getAllCamera = async () => {
     let response = await fetch('http://localhost:3000/api/cameras');
+    if(response.ok) {
+        return response.json();
+    } else {
+        console.error('server return', response.status);
+    }
+}
+
+const getAllTeddy = async () => {
+    let response = await fetch('http://localhost:3000/api/teddies');
+    if(response.ok) {
+        return response.json();
+    } else {
+        console.error ('server return', response.status);
+    }
+}
+
+const getAllFurniture = async () => {
+    let response = await fetch('http://localhost:3000/api/furniture');
     if(response.ok) {
         return response.json();
     } else {
@@ -19,58 +38,152 @@ const getAllCamera = async function() {
 
 
 // on créer un fonction, ici 'showAllCamera'
-// 
 
-const showAllCamera = () => {
-    getAllCamera().then((json) => {
-        json.forEach(element => {
-            createCamera(element)
-        });
-    })
-}
+    const showAllCamera = () => {
+        getAllCamera().then((json) => {
+            json.forEach(element => {
+                createCamera(element);
+            })
+        })
+    }
 
+    const showAllTeddy = () => {
+        getAllTeddy().then((json) => {
+            json.forEach(element => {
+                createTeddy(element);
+            })
+        })
+    }
 
-// on créer un fonction ici 'createCamera'
-// avec la valeur 'camera'
-
-// 
-
-const createCamera = (camera) => {
-    console.log(camera);
-
-    let firstDiv = document.createElement('div');      firstDiv.classList.add('produit');
-    let firstH2 = document.createElement('h2');        firstH2.classList.add('titre-produit');
-    let firstImg = document.createElement('img');   firstImg.classList.add('img-produit');
-    let secondDiv = document.createElement('div');  secondDiv.classList.add('info-produit');
-    let firstP = document.createElement('p');       firstP.classList.add('description-produit');
-    let secondP = document.createElement('p');      secondP.classList.add('prix-produit');
-    let thirdDiv = document.createElement('div');    thirdDiv.classList.add('button');
-    let firstButton = document.createElement('button');
-    let thirdP = document.createElement('p');     thirdP.classList.add('taille-produit');
-
-    document.getElementById('produits').appendChild(firstDiv);
-
-    firstImg.src = camera.imageUrl;
-
-    firstDiv.appendChild(firstH2);
-    firstDiv.appendChild(firstImg);
-    firstDiv.appendChild(secondDiv);
-    secondDiv.appendChild(firstP);
-    secondDiv.appendChild(secondP);
-    firstDiv.appendChild(thirdDiv);
-    thirdDiv.appendChild(firstButton);
-    secondDiv.appendChild(thirdP);
+    const showAllFurniture = () => {
+        getAllFurniture().then((json) => {
+            json.forEach(element => {
+                createFurniture(element);
+            })
+        })
+    }
 
 
-    firstH2.innerHTML = camera.name;
-    firstP.innerHTML = camera.description;
-    secondP.innerHTML = camera.price;
-    firstButton.innerHTML = 'Ajouter au Panier';
-    thirdP.innerHTML = camera.lenses[0];
+    const createCamera = (camera) => {
+
+        // console.log(camera);
     
+        let firstDiv = document.createElement('div');      firstDiv.classList.add('produit');
+        let firstH2 = document.createElement('h2');        firstH2.classList.add('titre-produit');
+        let firstImg = document.createElement('img');   firstImg.classList.add('img-produit');
+        let firstP = document.createElement('p');       firstP.classList.add('description-produit');
+        let firstButton = document.createElement('button');
+        let secondDiv = document.createElement('div');  secondDiv.classList.add('image');
+        let secondP = document.createElement('p');      secondP.classList.add('prix-produit');
+        let thirdDiv = document.createElement('div');  thirdDiv.classList.add('info-produit');
+        let thirdP = document.createElement('p');     thirdP.classList.add('taille-produit');
+        let fourDiv = document.createElement('div');    fourDiv.classList.add('button');
 
-}
+        document.getElementById('produits').appendChild(firstDiv);
+    
+        firstImg.src = camera.imageUrl;
+
+        firstDiv.appendChild(firstH2);
+        firstDiv.appendChild(secondDiv);
+        firstDiv.appendChild(thirdDiv);
+        firstDiv.appendChild(fourDiv);
+        secondDiv.appendChild(firstImg);
+        thirdDiv.appendChild(firstP);
+        thirdDiv.appendChild(secondP);
+        thirdDiv.appendChild(thirdP);
+        fourDiv.appendChild(firstButton);
+    
+        firstH2.innerHTML = camera.name;
+        firstP.innerHTML = camera.description;
+        firstButton.innerHTML = 'Ajouter au Panier';
+        secondP.innerHTML = camera.price;
+        thirdP.innerHTML = camera.lenses[0];
+        
+    }
+
+    const createTeddy = (teddy) => {
+
+        // console.log(teddy);
+    
+        let firstDiv = document.createElement('div');      firstDiv.classList.add('produit');
+        let firstH2 = document.createElement('h2');        firstH2.classList.add('titre-produit');
+        let firstImg = document.createElement('img');   firstImg.classList.add('img-produit');
+        let firstP = document.createElement('p');       firstP.classList.add('description-produit');
+        let firstButton = document.createElement('button');
+        let secondDiv = document.createElement('div');  secondDiv.classList.add('image');
+        let secondP = document.createElement('p');      secondP.classList.add('prix-produit');
+        let thirdDiv = document.createElement('div');  thirdDiv.classList.add('info-produit');
+        let thirdP = document.createElement('p');     thirdP.classList.add('taille-produit');
+        let fourDiv = document.createElement('div');    fourDiv.classList.add('button');
+
+        document.getElementById('produits').appendChild(firstDiv);
+    
+        firstImg.src = teddy.imageUrl;
+
+        firstDiv.appendChild(firstH2);
+        firstDiv.appendChild(secondDiv);
+        firstDiv.appendChild(thirdDiv);
+        firstDiv.appendChild(fourDiv);
+        secondDiv.appendChild(firstImg);
+        thirdDiv.appendChild(firstP);
+        thirdDiv.appendChild(secondP);
+        thirdDiv.appendChild(thirdP);
+        fourDiv.appendChild(firstButton);
+    
+        firstH2.innerHTML = teddy.name;
+        firstP.innerHTML = teddy.description;
+        firstButton.innerHTML = 'Ajouter au Panier';
+        secondP.innerHTML = teddy.price;
+        thirdP.innerHTML = teddy.colors[0];
+        
+    }
+
+    const createFurniture = (furniture) => {
+
+        console.log(furniture);
+    
+        let firstDiv = document.createElement('div');      firstDiv.classList.add('produit');
+        let firstH2 = document.createElement('h2');        firstH2.classList.add('titre-produit');
+        let firstImg = document.createElement('img');   firstImg.classList.add('img-produit');
+        let firstP = document.createElement('p');       firstP.classList.add('description-produit');
+        let firstButton = document.createElement('button');
+        let secondDiv = document.createElement('div');  secondDiv.classList.add('image');
+        let secondP = document.createElement('p');      secondP.classList.add('prix-produit');
+        let thirdDiv = document.createElement('div');  thirdDiv.classList.add('info-produit');
+        let thirdP = document.createElement('p');     thirdP.classList.add('taille-produit');
+        let fourDiv = document.createElement('div');    fourDiv.classList.add('button');
+
+        document.getElementById('produits').appendChild(firstDiv);
+    
+        firstImg.src = furniture.imageUrl;
+
+        firstDiv.appendChild(firstH2);
+        firstDiv.appendChild(secondDiv);
+        firstDiv.appendChild(thirdDiv);
+        firstDiv.appendChild(fourDiv);
+        secondDiv.appendChild(firstImg);
+        thirdDiv.appendChild(firstP);
+        thirdDiv.appendChild(secondP);
+        thirdDiv.appendChild(thirdP);
+        fourDiv.appendChild(firstButton);
+    
+        firstH2.innerHTML = furniture.name;
+        firstP.innerHTML = furniture.description;
+        firstButton.innerHTML = 'Ajouter au Panier';
+        secondP.innerHTML = furniture.price;
+        thirdP.innerHTML = furniture.varnish[0];
+        
+    }
+
+const input = document.getElementById('search');
+const log = document.getElementById('produits');
+
+input.addEventListener('change', () => {
+
+})
 
 // on affiche le contenue
 
-showAllCamera();
+showAllFurniture();
+// showAllCamera();
+// showAllTeddy();
