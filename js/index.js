8e72030e9dea0085eef82a117e4a25eaab91abdb
Romain Lavaur <romainlavaur@hotmail.com>
2021-02-08 00:54:09 +0100

    commit-19

--- js/index.js
+++ js/index.js
@@ -65,90 +65,26 @@ const showAllFurniture = () => {
   });
 };
 
-/*-------------- Afficher les Objets --------------*/
-
-
 /***** Barre de recherche *****/
 
 const selectElement = document.getElementById("product");
 
 selectElement.addEventListener("change", (event) => {
-  if (
-    event.target.value === "camera" ||
-    event.target.value === "cameras" ||
-    event.target.value === "Camera" ||
-    event.target.value === "Cameras" ||
-    event.target.value === "Zurss" ||
-    event.target.value === "zurss" ||
-    event.target.value === "Hirsch" ||
-    event.target.value === "hirsch" ||
-    event.target.value === "Franck" ||
-    event.target.value === "franck" ||
-    event.target.value === "Kuros" ||
-    event.target.value === "kuros" ||
-    event.target.value === "Katatone" ||
-    event.target.value === "katatone"
-  ) {
+  if (/(cameras?|zurss?|hirsch?|franck?|kuros?|katatone?)/i) {
     showAllCamera();
-  }
-
+    return;
+  } 
+  
   if (
-    event.target.value === "teddy" ||
-    event.target.value === "Teddy" ||
-    event.target.value === "ours" ||
-    event.target.value === "Ours" ||
-    event.target.value === "Oursons" ||
-    event.target.value === "oursons" ||
-    event.target.value === "Ourson" ||
-    event.target.value === "ourson" ||
-    event.target.value === "peluches" ||
-    event.target.value === "peluche" ||
-    event.target.value === "Peluches" ||
-    event.target.value === "peluches"
+    /(teddy|ours|oursons?|peluches?|norbert|arnold|lenny and carl|gustav|garfunkel)/i
   ) {
     showAllTeddy();
+    return;
   }
-
-  if (
-    event.target.value === "furniture" ||
-    event.target.value === "furnitures" ||
-    event.target.value === "Furniture" ||
-    event.target.value === "Furnitures" ||
-    event.target.value === "meuble" ||
-    event.target.value === "meubles" ||
-    event.target.value === "Meuble" ||
-    event.target.value === "Meubles" ||
-    event.target.value === "table" ||
-    event.target.value === "Table" ||
-    event.target.value === "tables" ||
-    event.target.value === "Tables" ||
-    event.target.value === "chaise" ||
-    event.target.value === "chaises" ||
-    event.target.value === "Chaises" ||
-    event.target.value === "chaise"
-  ) {
+  
+  if (/(furnitures?|meubles?|tables?|chaises?|planches?)/i) {
     showAllFurniture();
+    return;
   }
 });
 
-/***** Au click *****/
-
-// const prdt = document.querySelectorAll("#info");
-
-// prdt.forEach((element) => {
-//   element.addEventListener('click', (e) => {
-//     let camera = document.querySelector(".produit1")
-//     let teddy = document.querySelector(".produit2")
-//     let furniture = document.querySelector(".produit3")
-//     if (camera) {
-//       return showAllCamera();
-//     } else if (teddy) {
-//       return showAllTeddy();
-//     } else if (furniture){
-//       return showAllFurniture();
-//     };
-//     console.log(camera);
-//   });
-// });
-
-// console.log(prdt);
--- js/panier.js
+++ js/panier.js
@@ -1,48 +1,49 @@
 /***** mettre les produits dans le panier *****/
 
-let produitAjouter = document.getElementById("pdrt-ajouter");
-let boutton = document.querySelector("#submit");
-
 function getProduct() {
   let allProduit = JSON.parse(localStorage.getItem("produit"));
-  for (let i = 0; i < allProduit.length; i++) {
-    const produit = JSON.parse(localStorage.getItem('produit', JSON.stringify(allProduit[i])));
 
-    console.log(produit);
+  for (let i = 0; i < allProduit.length; i++) {
+    let produitAjouter = document.getElementById("pdrt-ajouter");
+    const produit = JSON.parse(
+      localStorage.getItem("produit", JSON.stringify(allProduit[i]))
+    );
+
+    console.log(produit[i]);
 
     /***** Les Produits dans le Panier *****/
     produit.forEach((element) => {
       prixTotal.innerHTML = `${element.price * element.quantity} €`;
-  
-        produitAjouter.innerHTML = `<div class="produit seul" id="produitSeul"><h3 class="titre-produit">${element.name}</h3><div class="image"><img src="${element.image}" alt="Image du produit mit dans le panier" width ="100%"></div><div class="Suprimer"><input type="number" value="${element.quantity}" class="quant"><button id="suprimerProduit" class="supre">Suprimer</button></div><p>Prix : ${element.price} €</p></div>`;
-      
+
+      /***** Afficher les produits *****/
+      produitAjouter.innerHTML = `<div class="produit seul" id="produitSeul"><h3 class="titre-produit">${element.name}</h3><div class="image"><img src="${element.image}" alt="Image du produit mit dans le panier" width ="100%"></div><div class="Suprimer"><input type="number" value="${element.quantity}" class="quant"><button id="suprimerProduit" class="supre">Suprimer</button></div><p>Prix : ${element.price} €</p></div>`;
+
+      // supprimer produit par produit
       let supProduct = document.getElementById("suprimerProduit");
-  
+
       supProduct.addEventListener("click", (e) => {
         if (element.quantity > 0) {
+          localStorage.getItem("");
           element.quantity -= 1;
-          console.log('produit suprimer');
+          console.log("produit suprimer");
         } else if (element.quantity === 0) {
-          localStorage.removeItem('produit')
-          console.log('le produit n\'est plus dans le panier');
+          localStorage.removeItem("produit");
+          console.log("le produit n'est plus dans le panier");
           return;
         }
         console.log(element.quantity);
-  
       });
-  
+
+      // vider le panier
       let suprimer = document.getElementById("suprimer");
       suprimer.addEventListener("click", (e) => {
-        localStorage.removeItem("produit");
-        localStorage.removeItem("quantité de produit");
+        localStorage.clear();
         confirm(
           `Vous avez suprimer ${element.quantity} produit du panier 😱😱😱`
         );
-        console.log('votre panier et vide');
+        console.log("votre panier et vide");
       });
     });
-    // console.log(produit[0]);
-    
   }
 
   // pas d'objet dans le panier
@@ -52,7 +53,33 @@ function getProduct() {
   } else {
     /***** Afficher le Prix *****/
     let prixTotal = document.getElementById("prixTotal");
-
   }
 }
 getProduct();
+
+/***** Valider le Formulaire *****/
+
+function valid() {
+  let regexName = /[^a-zA-Z]+[a-z]/;
+  let regexNameAndNumber = /[[:alnum:]]/i;
+  let regexMail = /[a-zA-Z0-9]+@.+[a-z]\.[a-z]{2,4}/;
+
+  let submit = document.getElementById('submit');
+  submit.addEventListener('click', (e) => {
+    if (
+      document.commande.lastName.value == regexName &&
+      document.commande.firstName.value == regexName &&
+      document.commande.adress.value == regexNameAndNumber &&
+      document.commande.city.value == regexName &&
+      document.commande.mail.value == regexMail 
+    ) {
+      document.commande.submit();
+      confirm('merci d\'avoir passer commande');
+      return;
+  
+    } else{
+      alert("remplisser correctement le formulaire");
+    }
+  })
+}
+valid();
